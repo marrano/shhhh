f063b72f7aac67d829eeaa8c74574bb09171ac08
Stefano Burchietti <stefano@nontelodiromai.it>
2012-12-11 09:13:24 +0100

    + ‘.classpath’ + ‘.project’ + ‘lib/jcurses-0.9.5.3-sources/jcurses/event/WindowEvent.java’ + ‘lib/jcurses-0.9.5.3-sources/jcurses/event/WindowListener.java’ + ‘lib/jcurses-0.9.5.3-sources/jcurses/event/WindowListenerManager.java’ + ‘src/org/stefano/Main.java’ + ‘src/org/stefano/MessageBox.java’ + ‘src/org/stefano/MyConsole.java’

---
+++ src/org/stefano/Main.java
@@ -0,0 +1,18 @@
+package org.stefano;
+
+//import jcurses.system.CharColor;
+//import jcurses.widgets.DefaultLayoutManager;
+//import jcurses.widgets.Label;
+//import jcurses.widgets.WidgetsConstants;
+//import jcurses.widgets.Window;
+
+
+public class Main 
+{
+    public static void main(String[] args)
+    {    
+        MyConsole w = new MyConsole(100, 40);
+        w.init();
+    }
+}
+
---
+++ src/org/stefano/MyConsole.java
@@ -0,0 +1,116 @@
+package org.stefano;
+
+import jcurses.event.*;
+import jcurses.system.CharColor;
+import jcurses.system.Toolkit;
+import jcurses.util.Message;
+import jcurses.util.Rectangle;
+import jcurses.widgets.*;
+
+
+public class MyConsole extends Window implements ItemListener, ActionListener, ValueChangedListener, WindowListener, WidgetsConstants 
+{
+  
+  static TextField textfield 	= null;
+  static Button button 			= null;
+  static TextArea textAreaOutput= null;
+  static TextArea textAreaInput = null;
+  static MyConsole window 		= null;
+  static CharColor defColor 	= null;
+  static CharColor defInvColor 	= null;
+
+  public MyConsole(int width, int height) {
+    super(Toolkit.getScreenWidth()-1, Toolkit.getScreenHeight()-1, false, ""); 
+    
+    defColor = new CharColor(CharColor.BLACK, CharColor.WHITE);
+    defInvColor = new CharColor(CharColor.WHITE, CharColor.BLACK);
+    
+    Toolkit.clearScreen(defColor); /*All black*/
+    
+    addListener((WindowListener) this);
+  }
+ 
+  public void init() {
+	DefaultLayoutManager mgr = new DefaultLayoutManager();
+    mgr.bindToContainer(getRootPanel());
+    mgr.addWidget(
+        new Label("Shhhh!!! ..questa è una chat segreta!", defColor),
+        			2, 2, 40, 10,
+        			WidgetsConstants.ALIGNMENT_LEFT,
+        			WidgetsConstants.ALIGNMENT_TOP);
+    
+    
+    textAreaOutput = new TextArea(Toolkit.getScreenWidth()-4, Toolkit.getScreenHeight()/2 + 10,"...qui si legge");  
+    textAreaOutput.setColors(defColor);
+    textAreaOutput.setTextComponentColors(defInvColor);	/* focus color*/
+    mgr.addWidget(textAreaOutput, 0, 4, Toolkit.getScreenWidth()-4, Toolkit.getScreenHeight()/2 + 10,
+            WidgetsConstants.ALIGNMENT_LEFT,
+            WidgetsConstants.ALIGNMENT_TOP);
+    
+    textAreaInput = new TextArea(Toolkit.getScreenWidth()-4, Toolkit.getScreenHeight()/2 - 20,"...qui si scrive");  
+    textAreaInput.setColors(defColor);
+    textAreaInput.setTextComponentColors(defInvColor);	/* focus color*/
+    mgr.addWidget(textAreaInput, 0, Toolkit.getScreenHeight()/2 + 16, Toolkit.getScreenWidth()-4, Toolkit.getScreenHeight()/2 - 20,
+            WidgetsConstants.ALIGNMENT_LEFT,
+            WidgetsConstants.ALIGNMENT_TOP);
+    
+
+    
+
+//    textfield = new TextField(10);
+//    mgr.addWidget(textfield, 0, 0, 20, 20,
+//        WidgetsConstants.ALIGNMENT_CENTER,
+//        WidgetsConstants.ALIGNMENT_CENTER);
+
+    button = new Button("Esci");
+    button.setColors(defColor);
+    button.setFocusedButtonColors(defInvColor);	/* focus color*/
+    button.setShortCut('q');
+    button.addListener(this);
+    mgr.addWidget(button, 2, Toolkit.getScreenHeight()-12, 10, 10,
+            WidgetsConstants.ALIGNMENT_BOTTOM,
+            WidgetsConstants.ALIGNMENT_LEFT);
+      		
+    show();
+    //Toolkit.changeColors(this.getRectangle(), defColor);  /* Set background to black */
+    textAreaInput.getFocus();	
+  }
+
+  private void setTitleColors(short magenta) {
+	// TODO Auto-generated method stub
+	
+}
+
+public void actionPerformed(ActionEvent event) {
+    Widget w = event.getSource();
+    if (w == button) {
+      MessageBox msg = new MessageBox("Esci", "Sei sicuro mimmo?");
+      msg.setBorderColors(defColor);
+      msg.setTitleColors(defColor);
+      
+      msg.getRootPanel().setPanelColors(defColor);
+      
+      msg.show();
+      
+      if(msg.getExitStatus()==true)
+    	  close();
+    }
+  }
+
+  public void stateChanged(ItemEvent e) {   
+  }
+
+  public void valueChanged(ValueChangedEvent e) {
+
+  }
+
+  public void windowChanged(WindowEvent event) {
+    if (event.getType() == WindowEvent.CLOSING) {
+      event.getSourceWindow().close();
+      Toolkit.clearScreen(defColor);
+    }
+    if (event.getType() == WindowEvent.ACTIVATED) {
+    	Toolkit.changeColors(this.getRectangle(), defColor);  /* Set background to black */
+      }    
+  }
+}
