4e2c019746f6cee2b4e76f499e92618a5425b670
Stefano Burchietti <stefano@nontelodiromai.it>
2012-12-11 14:28:48 +0100

    / ‘bin/org/stefano/Main.class’ / ‘src/org/stefano/Main.java’

--- src/org/stefano/Main.java
+++ src/org/stefano/Main.java
@@ -14,14 +14,14 @@ public class Main
         MyConsole w = new MyConsole(100, 40);
         w.init();
         
-//        Thread.currentThread();
-//        try {
-//			Thread.sleep(3000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//        w.close();
+        Thread.currentThread();
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        w.close();
 
     }
 }
