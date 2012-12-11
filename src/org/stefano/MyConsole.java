df78a85a415a4f8e01075a03668debff6cb3df29
Stefano Burchietti <stefano@nontelodiromai.it>
2012-12-11 15:34:44 +0100

    / ‘bin/org/stefano/MyConsole.class’ / ‘src/org/stefano/MyConsole.java’

--- src/org/stefano/MyConsole.java
+++ src/org/stefano/MyConsole.java
@@ -130,6 +130,7 @@ public void valueChanged(ValueChangedEvent e) {
   @Override
 public void windowChanged(WindowEvent event) {
 	  textAreaOutput.setText("Siamo dentro!");
+	  textAreaOutput.notifyAll();
     if (event.getType() == WindowEvent.CLOSING) 
     {
       event.getSourceWindow().close();
