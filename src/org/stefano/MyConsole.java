f278aa3707ca92a53ec653b64fbabf21e00c641e
Stefano Burchietti <stefano@nontelodiromai.it>
2012-12-11 15:37:09 +0100

    / ‘bin/org/stefano/MyConsole.class’ / ‘src/org/stefano/MyConsole.java’

--- src/org/stefano/MyConsole.java
+++ src/org/stefano/MyConsole.java
@@ -91,7 +91,7 @@ public class MyConsole extends Window implements ItemListener, ActionListener, V
 
 @Override
 public void actionPerformed(ActionEvent event) {
-	gtextAreaOutput.setText("Siamo pigiati!");
+	textAreaOutput.setText("Siamo pigiati!");
 	this.paint();
     if (event.getSource() == button) {
       MessageBox msg = new MessageBox("Esci", "Sei sicuro mimmo?");
