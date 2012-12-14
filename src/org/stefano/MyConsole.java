993d3a25f8fd16dc61a249791fccb0a82377982d
marrano <stefano@nontelodiromai.it>
2012-12-14 16:47:01 +0100

    / ‘bin/org/stefano/MyConsole.class’ / ‘src/org/stefano/MyConsole.java’

--- src/org/stefano/MyConsole.java
+++ src/org/stefano/MyConsole.java
@@ -140,7 +140,8 @@ public void actionPerformed(ActionEvent event) {
     }
     if (event.getSource() == buttonSend) {
 		textAreaOutput.setText(textAreaOutput.getText() + "\n" + textFieldInput.getText());
-		byte[] encrypted = cipher.doFinal(textFieldInput.getText().getBytes());
+		textFieldInput.getText().getBytes();
+		byte[] encrypted = cipher.doFinal();
 		textFieldInput.setText("");
 		paint();
 		textFieldInput.getFocus();	
