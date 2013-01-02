4d2c25786b4cd94b159fb44ac5e7ed29efc1f3b5
marrano <stefano@nontelodiromai.it>
2013-01-02 11:44:38 +0100

    / ‘bin/org/stefano/MyConsole.class’ / ‘src/org/stefano/MyConsole.java’

--- src/org/stefano/MyConsole.java
+++ src/org/stefano/MyConsole.java
@@ -146,7 +146,7 @@ public void actionPerformed(ActionEvent event) {
 			byte[] decryptedText;
 			try {
 				encrypted = encoder.doFinal(textFieldInput.getText().getBytes("UTF8"));
-				decryptedText = cipher.doFinal(ciphertext);
+				decryptedText = decoer.doFinal(encrypted);
 				
 				textAreaOutput.setText(textAreaOutput.getText() + "\n" + encrypted.toString());
 				
